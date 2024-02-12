d111e10a6c891c2526ed8b1dfc3ce8643236e62b
valdenilsondev <valdenilsondev@gmail.com>
2024-02-12 16:27:58 -0300

    Cria projeto Deslizando por Aracaju
    Criando projeto Deslizando por Aracaju

---
+++ DeslizandoPorAracaju/Library/Bee/Android/Prj/IL2CPP/Il2CppBackup/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,56 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void teste_Start_mC5525EB52C38F35D9EB92C01AFEC3965F8BF090C (void);
+extern void teste_Update_m92E973356DFB9C3897B442B0343B3DF2C1474735 (void);
+extern void teste__ctor_m3E8B38C1DE801FEE12AB40D418D2E56738505F9E (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	teste_Start_mC5525EB52C38F35D9EB92C01AFEC3965F8BF090C,
+	teste_Update_m92E973356DFB9C3897B442B0343B3DF2C1474735,
+	teste__ctor_m3E8B38C1DE801FEE12AB40D418D2E56738505F9E,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	1146,
+	1146,
+	1146,
+	2289,
+	1146,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
